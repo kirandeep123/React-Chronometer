47ca4b821256245bcfd773497c8a10a563130e34
kirandeep123 <me@kiran.cc>
2019-07-25 20:44:08 +0530

    Updates

---
+++ precache-manifest.c8e38860519990cc30c340340cdfeefc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819b0fc8e8804bb6ac19f5884176af43",
+    "url": "/React-Chronometer/index.html"
+  },
+  {
+    "revision": "59af2c9650e5416f0f94",
+    "url": "/React-Chronometer/static/css/main.5f58dfdd.chunk.css"
+  },
+  {
+    "revision": "420f875e568c800063c3",
+    "url": "/React-Chronometer/static/js/2.4f86a2a8.chunk.js"
+  },
+  {
+    "revision": "59af2c9650e5416f0f94",
+    "url": "/React-Chronometer/static/js/main.4a151224.chunk.js"
+  },
+  {
+    "revision": "4845cd63d91547c77901",
+    "url": "/React-Chronometer/static/js/runtime~main.be3626cf.js"
+  }
+]);
